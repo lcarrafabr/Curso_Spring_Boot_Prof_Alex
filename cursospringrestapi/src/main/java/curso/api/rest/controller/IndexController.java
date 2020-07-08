120239665ac4658947896cbd2c1885d7bc013077
lcarrafabr <lcarrafa.br@gmail.com>
2020-07-08 17:16:53 -0300

    Fazendo o crud de usuarios / criando model de telefones e consertando o bug de recursividade

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -7,10 +7,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
@@ -50,6 +55,39 @@ public class IndexController {
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		String login = usuario.getLogin().toUpperCase();
+		usuario.setNome(usuario.getNome().toUpperCase());
+		usuario.setLogin(login);
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+//		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK); //<<<==== Aula do professor Alex
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo); //<<<==== Aula da AlgaWorks
+	}
+	
+	@PutMapping(value = "/{iduser}")
+	public ResponseEntity<Usuario> atualizar(@PathVariable Long iduser, @RequestBody Usuario usuario){
+		
+		String login = usuario.getLogin().toUpperCase();
+		usuario.setLogin(login);
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+//		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK); //<<<==== Aula do professor Alex
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioSalvo); //<<<==== Aula da AlgaWorks
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable("id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+
 	}
 	
 	
